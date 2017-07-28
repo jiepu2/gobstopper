caa310e3e18df5d9d91cca4c0317cc4b5404beb0
Stefan Johnson <sj@scrimpycat.io>
2017-07-28 23:07:17 +1000

    Use new child_spec approach

--- apps/gobstopper_service/lib/gobstopper.service/auth.ex
+++ apps/gobstopper_service/lib/gobstopper.service/auth.ex
@@ -3,6 +3,14 @@ defmodule Gobstopper.Service.Auth do
 
     alias Gobstopper.Service.Auth.Identity
 
+    def child_spec(_args) do
+        %{
+            id: __MODULE__,
+            start: { __MODULE__, :start_link, [] },
+            type: :worker
+        }
+    end
+
     def start_link() do
         GenServer.start_link(__MODULE__, [], name: __MODULE__)
     end
